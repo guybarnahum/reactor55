bcaf0140b045bae5137be1d39ffd8dd523858564
guybarnahum <guy@barnahum.com>
2015-12-29 10:18:37 -0800

    fix up assets path

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,31 +5,31 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Bar-Nahum</title>
-        <link rel="icon" type="image/x-icon" href="{{ asset('images/favicons/favicon.ico') }}" />
-        <link rel="icon" type="image/png" href="{{ asset('images/favicons/favicon.png') }}" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicons/favicon.ico') }}" />
+        <link rel="icon" type="image/png" href="{{ asset('assets/images/favicons/favicon.png') }}" />
         <!-- For iPhone 4 Retina display: -->
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('images/favicons/apple-touch-icon-114x114-precomposed.png') }}">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('assets/images/favicons/apple-touch-icon-114x114-precomposed.png') }}">
         <!-- For iPad: -->
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('images/favicons/apple-touch-icon-72x72-precomposed.png') }}">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('assets/images/favicons/apple-touch-icon-72x72-precomposed.png') }}">
         <!-- For iPhone: -->
-        <link rel="apple-touch-icon-precomposed" href="{{ asset('images/favicons/apple-touch-icon-60x60-precomposed.png') }}">
+        <link rel="apple-touch-icon-precomposed" href="{{ asset('assets/images/favicons/apple-touch-icon-60x60-precomposed.png') }}">
         <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,400italic' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/theme.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/color-defaults.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-beige-black.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-black-beige.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-black-white.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-black-yellow.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-blue-white.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-green-white.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-red-white.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-white-black.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-white-blue.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-white-green.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-white-red.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/swatch-yellow-black.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/fonts.min.css') }}" media="screen">
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/theme.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/color-defaults.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-beige-black.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-black-beige.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-black-white.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-black-yellow.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-blue-white.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-green-white.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-red-white.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-white-black.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-white-blue.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-white-green.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-white-red.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swatch-yellow-black.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/fonts.min.css') }}" media="screen">
     </head>
     <body>
         <div id="content" role="main">
@@ -43,7 +43,7 @@
                                 <div class="box-dummy"></div>
                                 <figure class="box-inner">
                                     <img class="aligncenter size-full wp-image-3365" alt="blog-normal" 
-                                    src="{{ asset('images/people/GuyBarNahum.jpg') }}">
+                                    src="{{ asset('assets/images/people/GuyBarNahum.jpg') }}">
                                 </figure>
                                 <div class="hex-right"></div>
                                 <div class="hex-left"></div>
@@ -87,8 +87,8 @@
         <a class="go-top hex-alt" href="javascript:void(0)">
             <i class="fa fa-angle-up"></i>
         </a>
-        <script src="{{ asset('js/packages.min.js') }}""></script>
-        <script src="{{ asset('js/theme.min.js') }}""></script>
+        <script src="{{ asset('assets/js/packages.min.js') }}""></script>
+        <script src="{{ asset('assets/js/theme.min.js') }}""></script>
         
         <!-- Annotator -->
 
